0e3fe3095bd2c1c510e53f6eab9c411268cfa78a
lamiaelhbari <lamiaelhbari@gmail.com>
2024-03-24 12:28:21 +0000

    first commit

---
+++ indexDefault.ts
@@ -0,0 +1,36 @@
+/* The aim of this challenge is to correct the input errors detected by TypeScript 
+functionalities and to add the missing features so that we can encourage customers
+ to eat healthily. ===> See the update in the file index.ts  */ 
+
+// import restaurants from './restaurants';
+
+// const dollarSigns = '$$';
+// const deliveryTimeMax = '90';
+// const maxDistance = 10;
+// let result;
+
+// const priceBracket: string = dollarSigns.length;
+
+// const filteredRestaurants = restaurants.filter((restaurant) => {
+//   if (restaurant.priceBracket > priceBracket) {
+//     return false;
+//   }
+
+//   if (restaurant.deliveryTimeMinutes > deliveryTimeMax) {
+//     return false;
+//   }
+
+//   if (restaurant.distance > maxDistance) {
+//     return false;
+//   }
+
+//   return restaurant;
+// });
+
+// if (filteredRestaurants.length === 0) {
+//   result = 'There are no restaurants available right now.';
+// } else {
+//   result = `We found ${filteredRestaurants.length} restaurants, the first is ${filteredRestaurants[0].restaurantName}.`;
+// }
+
+// console.log(result);
